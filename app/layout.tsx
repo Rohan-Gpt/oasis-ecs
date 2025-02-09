9719ed08a77d2830bd258c2e48186c79ee4a9b29
Rohan Gupta <gupta.rohan0905@gmail.com>
2025-02-09 23:31:42 +0530

    improved notion code-block

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Toaster } from "@/components/ui/toaster";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -19,6 +21,7 @@ export default function RootLayout({
       <body className={inter.className}>
         {children}
         <SpeedInsights />
+        <Toaster />
       </body>
     </html>
   );
--- components/notion-renderer.tsx
+++ components/notion-renderer.tsx
@@ -4,12 +4,10 @@ import Link from "next/link";
 import dynamic from "next/dynamic";
 import { ExtendedRecordMap } from "notion-types";
 import "./styles/styles.css";
+import CodeBlock from "./CodeBlock";
 // import "prismjs/themes/prism-tomorrow.css";
 // import "katex/dist/katex.min.css";
 
-const Code = dynamic(() =>
-  import("react-notion-x/build/third-party/code").then((m) => m.Code)
-);
 const Collection = dynamic(() =>
   import("react-notion-x/build/third-party/collection").then(
     (m) => m.Collection
@@ -37,20 +35,84 @@ export const NotionPage = ({ recordMap, rootPageId }: NotionPageProps) => {
 
   return (
     <div className="notion__container">
-      <NotionRenderer
-        recordMap={recordMap}
-        fullPage={true}
-        darkMode={true}
-        rootPageId={rootPageId}
-        previewImages
-        components={{
-          nextLink: Link,
-          Code,
-          Collection,
-          Equation,
-          Modal,
-        }}
-      />
+      <div>
+        <NotionRenderer
+          recordMap={recordMap}
+          fullPage={true}
+          darkMode={true}
+          rootPageId={rootPageId}
+          previewImages
+          components={{
+            nextLink: Link,
+            Code: CodeBlock,
+            Collection,
+            Equation,
+            Modal,
+          }}
+        />
+      </div>
     </div>
   );
 };
+
+// "use client";
+// import { NotionRenderer as NotionRendererLib } from "react-notion-x";
+// // import { Code } from "react-notion-x/build/third-party/code";
+// import CodeBlock from "./CodeBlock";
+// import { ExtendedRecordMap } from "notion-types";
+
+// interface NotionPageProps {
+//   recordMap: ExtendedRecordMap;
+//   rootPageId: string;
+// }
+// // Week-4-1-647987d9b1894c54ba5c822978377910
+// export const NotionRenderer = ({ recordMap, rootPageId }: NotionPageProps) => {
+//   // const { resolvedTheme } = useTheme();
+
+//   return (
+//     <div className="w-full">
+//       <style>
+//         {`
+//           :root {
+//             --notion-font-family: "Poppins", sans-serif;
+//             --bg-color: #FAFAFA;
+//             --fg-color: #0a0a0a;
+//           }
+//           .dark-mode {
+//             --bg-color: #0a0a0a;
+//             --fg-color: #FAFAFA;
+//           }
+//           .notion-header {
+//             display: none !important;
+//           }
+//           .notion-code {
+//           border-radius: 12px;
+//           }
+
+//           .medium-zoom-image {
+//             border-radius: 0.5rem;
+//             border: 1px solid #0a0a0a36;
+//             background-size: cover;
+//             cursor: pointer;
+//           }
+//           .notion-page: {
+//             padding: 0px !important;
+//             background-color: transparent !important;
+
+//           }
+//         `}
+//       </style>
+//       <div>
+//         <NotionRendererLib
+//           components={{
+//             Code: CodeBlock,
+//           }}
+//           rootPageId={rootPageId}
+//           recordMap={recordMap}
+//           fullPage={true}
+//           darkMode={true}
+//         />
+//       </div>
+//     </div>
+//   );
+// };
